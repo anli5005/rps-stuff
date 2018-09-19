aeb4673106c7bcdab058049b9b5cd7f60fad6e94
Anthony Li <li.anthony917@gmail.com>
2018-09-19 11:41:54 -0400

    Remove types

--- src/@types/applescript.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'applescript' {
-  class AppleScript {
-    execString(script: string, callback: (error: Error, rtn: any) => void): void;
-  }
-  let as: AppleScript;
-  export default as;
-}
